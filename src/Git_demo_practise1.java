30fffd897ec084b9a9c656d5bbe8466cb086ffe2
sagarpangale <sagar@mobiuso.com>
2023-01-27 12:34:40 +0530

    changes made for asterik pattern

--- src/Git_demo_practise1.java
+++ src/Git_demo_practise1.java
@@ -7,7 +7,13 @@ public class Git_demo_practise1 {
 		
 		System.out.println("This is new demo for git");
 		System.out.println("This is new repo");
-
+			
+		System.out.println("New entered line from main branch");
+		for (int i=0 ;i<3 ; i++) {
+			System.out.println(" ");
+			System.out.println("***********");
+			
+		}
 	}
 
 }
