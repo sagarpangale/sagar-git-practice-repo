fc83bc138d70ca7e011fab627c4d7f3a05eb5e61
sagarpangale <sagar@mobiuso.com>
2023-01-27 12:10:58 +0530

    MAster file

---
+++ src/Git_demo_practise1.java
@@ -0,0 +1,13 @@
+
+public class Git_demo_practise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("This is new demo for git");
+		System.out.println("This is new repo");
+
+	}
+
+}
